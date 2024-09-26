438a6a2a40f55af87c56261f162daaa22874d799
Irina Karadimova <irinamaple84@gmail.com>
2024-09-26 21:55:39 +0100

    Update Homepage

---
+++ main.js
@@ -0,0 +1,12 @@
+// document.addEventListener("scroll", () => {
+//   const nav = document.querySelector("nav");
+//   let logo = document.getElementById("logo-nav");
+
+//   if (window.scrollY > 0 && logo.getAttribute("src") == "images/Logo-Nav.png") {
+//     nav.classList.add("scrolled");
+//     logo.setAttribute("src", "images/Logo-footer.png");
+//   } else {
+//     nav.classList.remove("scrolled");
+//     logo.setAttribute("src", "images/Logo-Nav.png");
+//   }
+// });
