3e18b7c1552b1b014abb1b2a4f8efef0ba3329ea
Irina Karadimova <irinamaple84@gmail.com>
2024-09-27 12:29:55 +0100

    Carousel Update

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// document.addEventListener("scroll", () => {
-//   const nav = document.querySelector("nav");
-//   let logo = document.getElementById("logo-nav");
+document.addEventListener("scroll", () => {
+  const nav = document.querySelector("nav");
+  let logo = document.getElementById("Logo-nav");
 
-//   if (window.scrollY > 0 && logo.getAttribute("src") == "images/Logo-Nav.png") {
-//     nav.classList.add("scrolled");
-//     logo.setAttribute("src", "images/Logo-footer.png");
-//   } else {
-//     nav.classList.remove("scrolled");
-//     logo.setAttribute("src", "images/Logo-Nav.png");
-//   }
-// });
+  if (window.scrollY > 0) {
+    nav.classList.add("scrolled");
+    logo.setAttribute("src", "images/Logo-footer.png");
+  } else {
+    nav.classList.remove("scrolled");
+    logo.setAttribute("src", "images/Logo-Nav.png");
+  }
+});
