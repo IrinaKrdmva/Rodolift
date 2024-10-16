9dea5d635c63819b0e36a55a9ed20bb2b567c8fb
Irina Karadimova <irinamaple84@gmail.com>
2024-10-16 16:38:19 +0100

    New Navbar

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener("scroll", () => {
   const nav = document.querySelector("nav");
-  let logo = document.getElementById("Logo-nav");
+  let logo = document.getElementById("logo-nav");
 
   if (window.scrollY > 0) {
     nav.classList.add("scrolled");
@@ -10,3 +10,13 @@ document.addEventListener("scroll", () => {
     logo.setAttribute("src", "images/Logo-Nav.jpg");
   }
 });
+
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
